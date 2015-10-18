c352362b7a4a70c915fd4eb1688a84cdf469a562
tomravalde <tomravalde@hotmail.com>
2015-10-18 13:18:32 +0100

    Updated names of files and .gitignore.

--- template/build-pdf.sh
+++ template/build-pdf.sh
@@ -11,9 +11,9 @@ Rscript knit.R
 
 # Convert Markdown to TeX
 # Note that you can't use --latex-engine here because it won't run bibtex
-if [ `hostname` = "cv-tr608-01" ]
+if [ `hostname` = "cv-tr608-01" ] ## Check if running on main author's machine (which contains the necessary dependencies)
 then
-	cp /home/tr608/ImpCol/PhD/my-refs.bib my-refs.bib
+	cp /home/tr608/ImpCol/PhD/my-refs.bib my-refs.bib ## Copies my main bib file of references
 	pandoc --template=/home/tr608/.pandoc/templates/tom.xelatex --natbib -o $FILENAME.tex $FILENAME.md 
 else
 	pandoc --template=default.latex --include-in-header=custom-local.sty --natbib -o $FILENAME.tex $FILENAME.md 
@@ -27,7 +27,6 @@ $ENGINE $FILENAME
 
 # Do the wordcount
 texcount -sub=section -html $FILENAME.tex > $WORDCOUNT
-
 
 # Show the file PDF and wordcount
 if [[ `uname` == 'Darwin' ]];
