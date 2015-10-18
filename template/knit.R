37df385c2e88d99c99334eedaa6184998cfd545d
tomravalde <tomravalde@hotmail.com>
2015-10-18 12:37:10 +0100

    Added build scripts from my paper directories.

---
+++ template/knit.R
@@ -0,0 +1,2 @@
+library(knitr)
+knit('paper.Rmd')
---
+++ template/make-doc.sh
@@ -0,0 +1,40 @@
+ENGINE=xelatex
+FILENAME=paper
+BROWSER=firefox
+WORDCOUNT=count.html
+
+# Update version control information
+sh vc
+
+# Knit the source file
+Rscript knit.R
+
+# Convert Markdown to TeX
+# Note that you can't use --latex-engine here because it won't run bibtex
+if [ `hostname` = "cv-tr608-01" ]
+then
+	cp /home/tr608/ImpCol/PhD/my-refs.bib my-refs.bib
+	pandoc --template=/home/tr608/.pandoc/templates/tom.xelatex --natbib -o $FILENAME.tex $FILENAME.md 
+else
+	pandoc --template=default.latex --include-in-header=custom-local.sty --natbib -o $FILENAME.tex $FILENAME.md 
+fi
+
+# Manually compile the file
+$ENGINE $FILENAME
+bibtex $FILENAME
+$ENGINE $FILENAME
+$ENGINE $FILENAME
+
+# Do the wordcount
+texcount -sub=section -html $FILENAME.tex > $WORDCOUNT
+
+
+# Show the file PDF and wordcount
+if [[ `uname` == 'Darwin' ]];
+then
+    open -a Preview $FILENAME.pdf
+    open -a Safari $WORDCOUNT
+else
+    acroread $FILENAME.pdf
+    firefox $WORDCOUNT
+fi
